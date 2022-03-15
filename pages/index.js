27b9c929dffa4a7ae325a5e0de1d42af144b69bc
Anderson David <andersondaviddev@gmail.com>
2022-03-15 12:05:46 -0300

    change index styles path

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Form from "../components/form";
-import * as S from './styles'
+import * as S from './styles/styles'
 
 export default function Home() {
   return (
